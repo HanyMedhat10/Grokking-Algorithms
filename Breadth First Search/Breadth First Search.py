fc491fde4b3e895df25456154b99f118730c5835
Hany Medhat <hany.medhat24@gmail.com>
2023-08-01 00:13:22 +0300

    done BFS

--- Breadth First Search/Breadth First Search.py
+++ Breadth First Search/Breadth First Search.py
@@ -1,3 +1,4 @@
+#  O (number of people + number of edges) = (V+E)
 """
 The algorithm works as follows:
 
@@ -19,20 +20,26 @@ graph["anuj"] = []
 graph["peggy"] = []
 graph["thom"] = []
 graph["jonny"] = []
-search_queue = deque()
-search_queue += graph["you"]
+
 def person_is_seller(name):
     return name[-1]=='m'
 
+def search(name):
+    search_queue = deque()
+    search_queue += graph[name]
+    searched = []
+    while search_queue:
+        person = search_queue.popleft()
+        if not person in searched:
+            if person_is_seller(person):
+                print(person + " is a mongo seller!")
+                return True
+            else:
+                search_queue +=graph[person]
+                searched.append(person)
+    return False
 
-while search_queue:
-    person = search_queue.popleft()
-    if person_is_seller(person):
-        print(person + "is a mongo seller!")
-        # return True
-    else:
-        search_queue +=graph[person]
-# return False
+
 # Chat GPT code
 def bfs(graph, start):
     visited = set()
